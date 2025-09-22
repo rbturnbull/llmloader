34877caabee8032dc701579b7b84b854b8ef8f3b
jamestq <james.quang@unimelb.edu.au>
2025-09-22 16:52:39 +1000

    :zap: fixed test to force azure call instead of openai call

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
-import pytest
+import pytest, os, warnings
 from langchain_core.messages import AIMessage   
 from unittest.mock import MagicMock, patch
+from dotenv import load_dotenv
 
 @pytest.fixture()
 def azure_mock_setup():
@@ -10,6 +11,18 @@ def azure_mock_setup():
         azure_mock.return_value = azure_mock_client
         azure_mock_client.invoke.return_value = AIMessage(content=prompt)      
         yield azure_mock, prompt
+
+@pytest.fixture()
+def force_azure_by_fail_openai():
+    load_dotenv()  # Load environment variables from .env file
+    prompt = "Write me a haiku about love"    
+    with patch("langchain_openai.ChatOpenAI") as openai_mock:
+        openai_mock.side_effect = Exception("OPENAI_API_KEY was set in environment variables. Ignoring the key...")
+        with patch("llmloader.azure.AzureChatOpenAI") as azure_mock:
+            azure_mock_client = MagicMock()
+            azure_mock.return_value = azure_mock_client
+            azure_mock_client.invoke.return_value = AIMessage(content=prompt)      
+            yield prompt
 
 @pytest.fixture()
 def credentials():
