b9d3f0bfe33d678c7b886dc4efdafc9e3c4579e1
jamestq <james.quang@unimelb.edu.au>
2025-09-22 16:21:22 +1000

    :zap: change api_endpoint variable to azure_endpoint

--- llmloader/azure.py
+++ llmloader/azure.py
@@ -23,13 +23,13 @@ class AzureOpenAILoader(Loader):
         # This is a custom endpoint deployed through Azure AI Foundry, which can include GPT, Grok, DeepSeek, Mistral, etc.
         api_key = api_key if api_key else os.getenv("AZURE_OPENAI_API_KEY", "")
         api_version = kwargs.pop("api_version", os.getenv("AZURE_OPENAI_API_VERSION", "2024-12-01-preview"))
-        api_endpoint = kwargs.pop("api_endpoint", os.getenv("AZURE_OPENAI_ENDPOINT", ""))    
+        azure_endpoint = kwargs.pop("api_endpoint", os.getenv("AZURE_OPENAI_ENDPOINT", ""))    
 
         return AzureChatOpenAI(
             azure_deployment=model,
             api_key=api_key,          
             api_version=api_version,
-            azure_endpoint=api_endpoint,
+            azure_endpoint=azure_endpoint,
             temperature=temperature,
             max_tokens=max_tokens,
             **kwargs,
